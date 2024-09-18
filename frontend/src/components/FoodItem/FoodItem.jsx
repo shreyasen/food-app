9ee7e4091a2785baebf9d37b0adfd34f9b2d8dc7
Shreya <ssen20170@gmail.com>
2024-09-18 16:32:31 +0530

    first commit

---
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,43 @@
+import { useContext, useState } from "react";
+import { assets } from "../../assets/assets";
+import "./FoodItem.css";
+import { StoreContext } from "../../context/StoreContext";
+
+const FoodItem = ({ id, name, price, description, image }) => {
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+
+  return (
+    <div className="food-item">
+      <div className="food-item-image-container">
+        <img className="food-item-image" src={image} alt={name} />
+        {!cartItems[id] ? (
+          <img
+            className="add"
+            src={assets.add_icon_white}
+            alt="add-item"
+            onClick={() => addToCart(id)}
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              src={assets.remove_icon_red}
+              onClick={() => removeFromCart(id)}
+            />
+            <p>{cartItems[id]}</p>
+            <img src={assets.add_icon_green} onClick={() => addToCart(id)} />
+          </div>
+        )}
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="rating" />
+        </div>
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">${price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default FoodItem;
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import Header from "../../components/Header/Header";
+import "./Home.css";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
+
+const Home = () => {
+  const [category, setCategory] = useState("all");
+  return (
+    <div>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
+    </div>
+  );
+};
+
+export default Home;
